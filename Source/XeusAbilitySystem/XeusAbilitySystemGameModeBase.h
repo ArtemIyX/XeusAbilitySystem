e65c880bb6f3c2b170c160250613d1bd785e38e0
Wellsaik <artem.podorozhko@outlook.com>
2023-11-14 11:07:09 +0300

    Unreal Engine - Blank Project

---
+++ Source/XeusAbilitySystem/XeusAbilitySystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "XeusAbilitySystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XEUSABILITYSYSTEM_API AXeusAbilitySystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
